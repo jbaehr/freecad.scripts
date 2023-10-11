27b3f4b287706e4dea685343503c7737b8530ee7
Jonas Bähr <jonas.baehr@web.de>
2023-10-11 23:20:08 +0200

    Make the fallback editor somewhat more python-code friendly
    The defaults of QPlainTextEdit are not usable at all for programming, so
    I implemented at least auto-indent, tab-expansion, a monospace font and
    no word-wrap.

--- freecad/scripts/editor.py
+++ freecad/scripts/editor.py
@@ -4,7 +4,7 @@ This module defines the python editor and related classes.
 The goal is to eventually expose FreeCAD's python editor, which is currently only available
 for C++. So here we have only a very basic substitute with no planes for fancy extensions.
 
-Maybe we'll use other fall backs for earler FC versions, like
+Maybe we'll use other fall backs for earlier FreeCAD versions, like
     https://github.com/mnesarco/FreeCAD_Utils/blob/main/freecad/mnesarco/utils/editor.py
     (GPLv3, i.e. must be installed separately)
 or
@@ -13,6 +13,32 @@ or
 """
 
 from PySide import QtGui
+from PySide.QtCore import Qt
 
 
-Editor = QtGui.QPlainTextEdit
+class FallbackEditor(QtGui.QPlainTextEdit):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setLineWrapMode(QtGui.QPlainTextEdit.NoWrap)
+        self.setFont(QtGui.QFont("Courier")) # is there some generic monospace font?
+
+    def keyPressEvent(self, event):
+        super().keyPressEvent(event)
+
+        # some basic auto-indent
+        if event.key() in (Qt.Key_Enter, Qt.Key_Return):
+            cursor = self.textCursor()
+            previousBlock = cursor.block().previous()
+            if previousBlock.isValid():
+                line = previousBlock.text()
+                indent = line[: len(line) - len(line.lstrip())]
+                self.insertPlainText(indent)
+
+        # some basic expand-tabs, using PEP8 recommentation of 4 spaces
+        if event.key() == Qt.Key_Tab:
+            cursor = self.textCursor()
+            cursor.deletePreviousChar()
+            self.insertPlainText(" " * 4)
+
+
+Editor = FallbackEditor
